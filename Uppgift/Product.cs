1bba756f84d6723876e19520ad3bed605c7b1671
xXEmulsk69420Xx <emil.jonsson2@elev.ga.ntig.se>
2023-02-21 09:07:27 +0100

    Add project files.

---
+++ Uppgift/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uppgift
+{
+    internal class Product
+    {
+        public 
+        public String Name { get; set; }
+
+        public decimal Price { get; set; }
+
+        public int Amount { get; set; }
+    }
+}
